b6dc878b40b5afcb9900a12776b1895823bf00ba
Tafadzwa-Chiwozva <tafadzwachiwozva@gmail.com>
2025-04-24 20:17:08 +0400

    Fix toast notification system by removing uuid dependency

--- src/contexts/ToastContext.jsx
+++ src/contexts/ToastContext.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
 import ToastContainer from '../components/ToastContainer';
 
 const ToastContext = createContext();
@@ -17,7 +16,7 @@ export const ToastProvider = ({ children }) => {
   const [toasts, setToasts] = useState([]);
 
   const addToast = useCallback((message, type = 'success', duration = 3000) => {
-    const id = uuidv4();
+    const id = Math.random().toString(36).substring(2, 9);
     const toast = {
       id,
       message,
